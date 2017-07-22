db622f50c5267ace2d8fec73b01d73b5b170132e
patkeav <keavenyp@gmail.com>
2017-07-21 18:31:37 -0600

    Added featured category image, altered breadcrumbs

--- patrick-keaveny-2017/category.php
+++ patrick-keaveny-2017/category.php
@@ -1,8 +1,17 @@
 <?php get_header(); ?>
 
+<? 
+	$ID = get_queried_object()->term_id; 
+
+	$feature_image = get_field('custom_category_image', 'category_' . $ID); print_r(get_fields()); 
+
+?>
+
 	<div class="content">
 
 		<div class="content-inner">
+
+			<div class="category-feature"><img src="<? echo $feature_image['sizes']['4-1-lg']; ?>" /></div>
 
 			<? get_template_part('/includes/_components/breadcrumbs.inc') ; ?>
 
@@ -16,9 +25,13 @@
 
 						<div class="post-grid-single flex-child">
 
-							<img src="<? bloginfo('template_url'); ?>/images/placeholder.jpg" class="post-grid-image" />
+							<a href="<? the_permalink(); ?>">
+								<img src="<? bloginfo('template_url'); ?>/images/placeholder.jpg" class="post-grid-image" />
+							</a>
 
-							<h3 class="post-grid-single-title"><? the_title(); ?></h3>
+							<h3 class="post-grid-single-title">
+								<a href="<? the_permalink(); ?>"><? the_title(); ?></a>
+							</h3>
 
 							<div class="post-grid-single-date"><? the_date('D, M j, Y'); ?></div> 
 
